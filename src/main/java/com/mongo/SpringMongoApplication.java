97d9570589d4a79af47f0f5f52fb3ce4fac1181a
Harsha <harshams159@gmail.com>
2022-11-11 15:21:25 +0530

    1st commit

--- src/main/java/com/mongo/SpringMongoApplication.java
+++ src/main/java/com/mongo/SpringMongoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringMongoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMongoApplication.class, args);
+		//1st commit
 	}
 
 }
